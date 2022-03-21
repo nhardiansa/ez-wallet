e27b49b4e3c67fa5788bdabcfb7cc8c4f4aca981
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-21 23:06:38 +0800

    feat: profile picture can update

--- helpers/http.js
+++ helpers/http.js
@@ -3,12 +3,16 @@ import constants from './constants';
 
 const {apiURL} = constants;
 
-export const axiosInstance = (useToken = false) => {
+export const axiosInstance = (useToken = false, multiPart=false) => {
   const headers = {};
 
   if (useToken) {
     const token = localStorage.getItem('token');
     headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (multiPart) {
+    headers['Content-Type'] = 'multipart/form-data';
   }
 
   return axios.create({
